da9b4846c9dec4f63c6dc44ebc7d4b109cafabc2
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2024-06-09 14:22:37 -0400

    Added Enemy Pathing

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public List<Transform> Waypoints;
+}
