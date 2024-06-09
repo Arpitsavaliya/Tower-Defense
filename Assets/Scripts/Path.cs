00fb325e1a58e5076afaf24608172ca5b089fa6d
Arpit Savaliya <arpitsavaliya11@gmail.com>
2024-06-09 16:36:20 -0400

    Enemy Movements and path finding scripts Basic GamePlay

--- Assets/Scripts/Path.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-
-public class Path : MonoBehaviour
-{
-    public List<Transform> Waypoints;
-}
